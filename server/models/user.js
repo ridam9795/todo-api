c0501538f0c85415ae609ba2d8e4d46538488fbd
rid <ridamnagar11@gmail.com>
2018-08-14 06:42:16 +0530

    Added login

--- server/models/user.js
+++ server/models/user.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
-const bcrypt=require('bcrypt');
+const bcrypt = require('bcrypt');
+
 var UserSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -32,10 +33,6 @@ var UserSchema = new mongoose.Schema({
   }]
 });
 
-
-
-
-
 UserSchema.methods.toJSON = function () {
   var user = this;
   var userObject = user.toObject();
@@ -43,50 +40,68 @@ UserSchema.methods.toJSON = function () {
   return _.pick(userObject, ['_id', 'email']);
 };
 
-
 UserSchema.methods.generateAuthToken = function () {
   var user = this;
   var access = 'auth';
   var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
 
-  user.tokens=user.tokens.concat([{access,token}]);
+  user.tokens.push({access, token});
 
   return user.save().then(() => {
     return token;
   });
 };
 
-UserSchema.statics.findByToken=function(token){
-  var User=this;
+UserSchema.statics.findByToken = function (token) {
+  var User = this;
   var decoded;
 
-  try{
-  decoded=jwt.verify(token,'abc123');
-  }catch(e){
-return Promise.reject();
-  }
-  return User.findOne({
-    '_id':decoded._id,
-    'tokens.token':token,
-    'tokens.access':'auth'
-  });
-};
-UserSchema.pre('save',function(next){
-  var user=this;
-  if(user.isModified('password')){
-bcrypt.genSalt(8,(error,salt)=>{
-  bcrypt.hash(user.password,salt,(error,hash)=>{
-    user.password=hash;
-    next();
-  });
-});
-  }else{
-    next()
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
+    return Promise.reject();
   }
 
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
+};
+UserSchema.statics.findByCredential=function(email,password){
+  var user=this;
+ 
+ return user.findOne({email}).then((user)=>{
+if(!user){
+  return Promise.reject();
+}
+return new Promise((resolve,reject)=>{
+  bcrypt.compare(password,user.password,(error,result)=>{
+         if(result){
+          resolve(user);
+         }else{
+           reject();
+         }
+         
+  })
 })
+ })
+}
+UserSchema.pre('save', function (next) {
+  var user = this;
+
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
 var User = mongoose.model('User', UserSchema);
-console.log(UserSchema.obj);
-console.log("Method :",UserSchema.methods);
 
 module.exports = {User}
--- server/server.js
+++ server/server.js
@@ -1,13 +1,15 @@
 require('./config/config');
-const {authenticate}=require('./middleware/authenticate');
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
-
+const bcrypt=require('bcrypt');
+const jwt=require('jsonwebtoken');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT;
@@ -95,17 +97,7 @@ app.patch('/todos/:id', (req, res) => {
     res.status(400).send();
   })
 });
-// app.post('/users',(req,res)=>{
-//   var body=_.pick(req.body,['email','password']);
-//   var user=new User(body);
-//   user.save().then(()=>{
-//     return user.generateAuthToken()
-//   }).then((token)=>{
-//     res.headers('x-auth',token).send(user)
-//   }).catch((e)=>{
-// res.status(400).send(e);
-//   })
-// });
+
 // POST /users
 app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
@@ -120,9 +112,23 @@ app.post('/users', (req, res) => {
   })
 });
 
-app.get('/users/me',authenticate,(req,res)=>{
- res.send(req.user)
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
+});
+app.post('/users/login',(req,res)=>{
+  var body=_.pick(req.body,['email','password']);
+  User.findByCredential(body.email,body.password).then((user)=>{
+return  user.generateAuthToken().then((token)=>{
+   res.header('x-auth',token).send(user);
+})  }).catch((e)=>{
+    res.status(404).send(e);
+  })
 })
+
+    
+    
+
+  
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
